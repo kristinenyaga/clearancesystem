cd8c7a46de830525a17c4a5dfa691267fa015bef
kristinenyaga <kristine.nyaga@student.moringaschool.com>
2022-07-21 22:34:59 +0300

    adds validation function

--- assets/js/touch login.js
+++ assets/js/touch login.js
@@ -1,3 +1,20 @@
+const { text } = require("stream/consumers");
+
 function validation (){
-    
+    var form =document.getElementById('formData')
+    var email=document.getElementById('studentem')
+    var pass=document.getElementById('studentpass')
+    var pattern=/^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if(email.match(pattern)){
+        form.classList.add("valid");
+        text.innerHtml="please enter valid email"
+        
+
+    }else{
+        form.classList.remove("valid")
+        
+
+        
+    }
+
 }
